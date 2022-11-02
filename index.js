bae1d087e795a1b58860d2a8ca2400c943383fa1
teakoVA <nleb641999@gmail.com>
2022-11-02 15:42:02 -0700

    added sql files

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('console.table');
 
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateRole: "Update Employee Role",
+    updateEmployeeManager: "Update Employee Manager",
+    viewAllRoles: "View All Roles",
+    exit: "Exit"
+};
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3001,
     user: 'root',
     password: 'chargers619',
-    database: 'employees'
+    database: 'employees_db'
 });
 
 connection.connect(err => {
